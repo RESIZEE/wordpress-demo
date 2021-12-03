c9b06578955a58fd1cc301127f9f2788bf7de087
Nikola Stankovic <disa_malii@hotmail.com>
2021-12-03 12:58:38 +0100

    JS modules added

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <?php get_header(); ?>
 
-    <div class="container">
-        <?php while(have_posts()){
-            the_post()
-            ?>
-
-        <?php } ?>
-    </div>
+<div class="container">
+    <?php while (have_posts()) {
+        the_post()
+    ?>
+        <h1><a href="<?php echo the_permalink(); ?>"><?php the_title(); ?></a></h1>
+        <?php the_content(); ?>
+    <?php } ?>
+</div>
 
 <?php get_footer() ?>
