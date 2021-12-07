27d09738bf67614b4e786d4450b75d395d87d3c3
Dusan Djordjevic <dussann1997@live.com>
2021-12-07 15:55:06 +0100

    Added dynamic header menu

--- header.php
+++ header.php
@@ -12,4 +12,22 @@
     </head>
     <body <?php body_class(); ?>>
         <header>
+            <nav class="navbar navbar-expand-lg bg-black navbar-dark py-4">
+                <div class="container">
+                    <a href="<?php echo get_home_url() ?>" class="navbar-brand">Logo</a>
+
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navmenu">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+
+                    <?php
+                    wp_nav_menu([
+                            'theme_location' => 'headerMenuLocation',
+                            'menu_class' => 'navbar-nav ms-auto',
+                            'container_class' => 'collapse navbar-collapse',
+                            'container_id' => 'navmenu',
+                    ]);
+                    ?>
+                </div>
+            </nav>
         </header>
