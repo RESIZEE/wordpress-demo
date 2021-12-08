5e45e815ded9b4ef19452a424af3bdf2c26fc3ac
Dusan Djordjevic <dussann1997@live.com>
2021-12-08 14:46:14 +0100

    Chagned custom to main query

--- inc/adjust-queries.php
+++ inc/adjust-queries.php
@@ -1,16 +1,17 @@
 <?php
 add_action('pre_get_posts', 'demo_adjust_queries');
 
-function demo_adjust_queries($query)
-{
+function demo_adjust_queries($query) {
 
-    if (
+    if(
         !is_admin() &&
-        is_main_query() && (is_post_type_archive('movie') ||
+        is_main_query() &&
+        (is_post_type_archive('movie') ||
             is_post_type_archive('book') ||
-            is_post_type_archive('game'))
+            is_post_type_archive('game')
+        )
     ) {
-        $posts_number = wp_is_mobile() ? 3 : 3;
+        $posts_number = wp_is_mobile() ? 3 : 9;
         $query->set('posts_per_page', $posts_number);
     }
 }
