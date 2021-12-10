5a9bba7e33874d327ccf1de456345a335d2443c1
Nikola Stankovic <disa_malii@hotmail.com>
2021-12-10 10:29:09 +0100

    side menu fixed

--- template-parts/genre-side-menu.php
+++ template-parts/genre-side-menu.php
@@ -1,38 +1,42 @@
-<!-- SIDE MENU -->
-<div class="col-lg-3 side-menu d-none d-lg-block">
-    <h4 class="mb-4"><?php echo __('Genres', 'demo') ?></h4>
-    <ul>
-        <?php
-        $allGameGenres = get_terms([
-            'taxonomy' => 'genre',
-        ]);
-        foreach ($allGameGenres as $genre) {
-        ?>
-            <li>
-                <a href="<?php echo esc_url(add_query_arg('genre', $genre->slug)) ?>">
-                    <?php echo $genre->name; ?>
-                </a>
-            </li>
-        <?php } ?>
-    </ul>
-</div>
-<!-- END OF SIDE MENU -->
+                    <!-- DROPDOWN MENU -->
+                    <div class="dropdown d-block d-md-none mb-5">
+                        <button class="dropdown-toggle col-12 text-start d-flex align-items-center justify-content-between px-3 py-2" type="button" id="categoriesmenu" data-bs-toggle="dropdown" aria-expanded="false">
+                            <h4 class="m-0"><?php echo __('Genres', 'demo') ?></h4>
+                            <span class="fs-3">+</span>
+                        </button>
+                        <ul class="dropdown-menu" aria-labelledby="categoriesmenu">
+                            <?php
+                            $posts_in_post_type = get_posts([
+                                'fields' => 'ids',
+                                'post_type' => get_post_type(),
+                                'posts_per_page' => -1,
+                            ]);
+                            /* PERFORMANCE WARNING */
+                            $allGenres = wp_get_object_terms($posts_in_post_type, 'genre', ['ids']);
+                            foreach ($allGenres as $genre) { ?>
+                                <li class="nav-item">
+                                    <a href="<?php echo esc_url(add_query_arg('genre', $genre->slug, get_post_type_archive_link(get_post_type()))) ?>" class="nav-link">
+                                        <?php echo $genre->name; ?>
+                                    </a>
+                                </li>
+                            <?php } ?>
+                        </ul>
+                    </div>
+                    <!-- END OF DROPDOWN MENU -->
 
-<!-- DROPDOWN MENU -->
-<div class="dropdown d-block d-md-none mb-5">
-    <button class="dropdown-toggle col-12 text-start d-flex align-items-center justify-content-between px-3 py-2" type="button" id="categoriesmenu" data-bs-toggle="dropdown" aria-expanded="false">
-        <h4 class="m-0"><?php echo __('Genres', 'demo') ?></h4>
-        <span class="fs-3">+</span>
-    </button>
-    <ul class="dropdown-menu" aria-labelledby="categoriesmenu">
-        <?php
-        foreach ($allGameGenres as $genre) { ?>
-            <li class="nav-item">
-                <a href="#" class="nav-link">
-                    <?php echo $genre->name; ?>
-                </a>
-            </li>
-        <?php } ?>
-    </ul>
-</div>
-<!-- END OF DROPDOWN MENU -->
+                    <!-- SIDE MENU -->
+                    <div class="col-lg-3 side-menu d-none d-lg-block">
+                        <h4 class="mb-4"><?php echo __('Genres', 'demo'); ?></h4>
+                        <ul>
+                            <?php
+                            foreach ($allGenres as $genre) {
+                            ?>
+                                <li class="<?php echo get_query_var('genre') === $genre->slug ? 'active' : '' ?>">
+                                    <a href="<?php echo esc_url(add_query_arg('genre', $genre->slug, get_post_type_archive_link(get_post_type()))) ?>">
+                                        <?php echo $genre->name; ?>
+                                    </a>
+                                </li>
+                            <?php } ?>
+                        </ul>
+                    </div>
+                    <!-- END OF SIDE MENU -->
