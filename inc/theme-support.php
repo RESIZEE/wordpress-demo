29c26b2f894757355b9e00ce6b831453c3c1deb8
Nikola Stankovic <disa_malii@hotmail.com>
2021-12-10 11:28:59 +0100

    Added custom logo feature

--- inc/theme-support.php
+++ inc/theme-support.php
@@ -1,11 +1,21 @@
 <?php
 
 add_action('after_setup_theme', 'demo_features');
-function demo_features() {
+function demo_features()
+{
     //Post-type features
     add_theme_support('post-thumbnails');
 
     //Image sizes
     add_image_size('card-image', 0, 500, true);
     add_image_size('single-image', 0, 450, true);
+
+    //Custom Logo
+    add_theme_support('custom-logo', [
+        'height' => 100,
+        'width' => 400,
+        'flex-height' => true,
+        'flex-width' => true,
+        'unlink-homepage-logo' => true,
+    ]);
 }
