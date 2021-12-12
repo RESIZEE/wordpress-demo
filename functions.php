990e9e14f9d2925fa698908ec5ed8a38b9b2f34b
Dusan Djordjevic <dusan.djordjevic.biz@gmail.com>
2021-12-12 15:42:39 +0100

    Changed directory structure

--- functions.php
+++ functions.php
@@ -1,11 +1,11 @@
 <?php
 
-require 'inc/custom-post-types.php';
-require 'inc/enqueue.php';
-require 'inc/templates.php';
-require 'inc/menus.php';
-require 'inc/theme-support.php';
-require 'inc/adjust-queries.php';
-require 'inc/helpers.php';
-require 'inc/review-resources.php';
-require 'inc/filters.php';
+require_once 'inc/custom-post-types.php';
+require_once 'inc/enqueue.php';
+require_once 'inc/templates.php';
+require_once 'inc/menus.php';
+require_once 'inc/theme-support.php';
+require_once 'inc/adjust-queries.php';
+require_once 'inc/helpers.php';
+require_once 'inc/review-resources.php';
+require_once 'inc/filters.php';
